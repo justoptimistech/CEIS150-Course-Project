5aaed7828fd16702536ecb21dc6cc6258b8b604b
Sovanarry Top <155851957+justoptimistech@users.noreply.github.com>
2024-02-24 10:45:24 -0500

    Update Stock_menu.py

--- Stock_menu.py
+++ Stock_menu.py
@@ -126,9 +126,16 @@ def display_chart(stock_list):
     print("This method is under construction")
   
                 
- # Get price and volume history from Yahoo! Finance using CSV import.
-def import_stock_csv(stock_list):
-    print("This method is under construction")
+# Get price and volume history from Yahoo! Finance using CSV import.
+def import_stock_csv(self,stock_list,symbol,filename):
+        for stock in stock_list:
+                if stock.symbol == symbol:
+                    with open(filename, newline='') as stockdata:
+                        datareader = csv.reader(stockdata,delimiter=',')
+                        next(datareader)
+                        for row in datareader:
+                            daily_data = DailyData(str(row[0]),float(row[4]),float(row[6]))
+                            stock.add_data(daily_data) 
     
    # Display Report for All Stocks
 def display_report(stock_list):
